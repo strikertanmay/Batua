2aab6bf033bbb14f0990e31a343e6a3401d78a2e
Tanmay Mittal <iec2017017@iiita.ac.in>
2019-10-12 21:02:55 +0530

    expo setup again

--- frontend/App.js
+++ frontend/App.js
@@ -1,47 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import {AppLoading, Asset} from 'expo';
-import Navigation from './navigation';
-import {Block} from './components';
-import * as constants from './constants';
-
-const images = [
-    require('./assets/images/icon.png')
-];
-
-export default class App extends React.Component  {
-  state = {
-    isLoadingComplete :  false
-  }
-
-  handleResourseAsync = async() => {
-    
-    const cacheImages = images.map(img => {
-        return Asset.fromModule(image).downloadAsync()
-      });
-
-      return Promise.all(cacheImages);
-  };
-
-  render(){
-
-    if(!this.state.isLoadingComplete && !this.props.skipLoadingScreen){
-        return(
-          <AppLoading
-            startAsync = {this.handleResourseAsync}
-            onError = {error => console.warn(error)}
-            onFinish = { () => this.setState({isLoadingComplete : true})}
-          
-          />
-        );
-    }
-       return (
-        <View style={styles.container}>
-          <Navigation />
-        </View>
-      );
-    };
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
