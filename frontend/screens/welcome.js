7434530c09e93c98d7ca6081033b8f735c644834
Tanmay Mittal <iec2017017@iiita.ac.in>
2019-10-12 17:39:48 +0530

    welcomescreen added

--- frontend/screens/welcome.js
+++ frontend/screens/welcome.js
@@ -7,17 +7,42 @@ export default class Welcome extends Component {
     static navigationOptions = {
         header : null
     }
+
+    renderIllustrations() {
+        return(
+            <Block>
+                <Text>Image</Text>
+            </Block>
+        )
+    }
+
+    render(){
+        return(
+            <Block>
+                <Text>* * *</Text>
+            </Block>
+        )
+    }
     render() {
         return(
-            <Block center midddle>
-                <Block center middle>
+            <Block >
+                <Block center middle flex = {0.3}>
                     <Text h1 center bold>One Family
                     <Text h1 primary>One Account</Text>
                     </Text>
                     <Text h3 gray2 style = {{marginTop : themes.size.padding/2}}>Pay Exacty the way you want</Text>
                 </Block>
-                <Text>Welcome</Text>
+            <Block center middle>
+                {this.renderIllustrations()}
+                {this.renderSreps()}
             </Block>
+            <Block middle flex={0.5} margin= {[0, themes.size.padding = 2]}>
+                <Button gradient onPress = {}>
+                    <Text center semibold white>Go Ahead</Text>
+                </Button>
+            </Block>
+            </Block>
+
         )
     }
 }
