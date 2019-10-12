a819f676efd7e11d3781b3c5dc2fe41d46e9e829
Tanmay Mittal <iec2017017@iiita.ac.in>
2019-10-13 02:59:56 +0530

    some more pages added

---
+++ frontend/screens/Showtransaction.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  TouchableOpacity
+} from "react-native";
+
+import RoundButton from "../component/RoundButton";
+
+const getStartedHandler = (navigation) => {
+  navigation.navigate('Login');
+};
+
+const Addmoney = props => {
+  return (
+    <View style={styles.Container}>
+      <View>
+        <Image
+          source={require("../assets/wallet.png")}
+          style={styles.background_image}
+        />
+      </View>
+        <View>
+            <Text>User Family</Text>
+        </View>
+        <View>
+            <Text>ADMIN</Text>
+        </View>
+        <View>
+            <Text>Ayush Joglekar</Text>
+        </View>
+      <View>
+          <FlatList />
+      </View>
+    </View>
+  );
+};
+
+export default Addmoney;
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  background_image: {
+    width: 80,
+    height: 120,
+    resizeMode: "stretch"
+  },
+  get_started: {
+    marginTop: 30
+  }
+});
